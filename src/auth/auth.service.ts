60ce2a22543fbbd35197b0d6973acb7eb1d239ad
Ibrar Ali <ibrar.ali4256@gmail.com>
2024-09-09 18:07:09 +0500

    Transactions and proper error handling Added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersRepository } from './auth.repository';
 import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
 import * as bcrypt from 'bcrypt';
@@ -14,22 +18,32 @@ export class AuthService {
   ) {}
 
   async signUp(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
-    return this.usersRepository.createUser(authCredentialsDTO);
+    try {
+      return this.usersRepository.createUser(authCredentialsDTO);
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to create user');
+    }
   }
 
   async signIn(
     authCredentialsDTO: AuthCredentialsDTO,
   ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDTO;
+    try {
+      const user = await this.usersRepository.findOne({ where: { username } });
 
-    const user = await this.usersRepository.findOne({ where: { username } });
-
-    if (user && (await bcrypt.compare(password, user.password))) {
-      const payload: jwtPayload = { username };
-      const accessToken: string = await this.jwtService.sign(payload);
-      return { accessToken };
-    } else {
-      throw new UnauthorizedException('Please check your login credentials');
+      if (user && (await bcrypt.compare(password, user.password))) {
+        const payload: jwtPayload = { username };
+        const accessToken: string = await this.jwtService.sign(payload);
+        return { accessToken };
+      } else {
+        throw new UnauthorizedException('Please check your login credentials');
+      }
+    } catch (error) {
+      if (error instanceof UnauthorizedException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Failed to sign in');
     }
   }
 }
